9e8ec9ad0b5b76fd3af2afc3f9e26091f33efe11
kandalog <asuasu.tee@gmail.com>
2025-08-01 22:20:51 +0900

    Fix eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "warn",
+    },
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
