7c444c6045cd48fa064d23a532144344ebbc0aad
kandalog <asuasu.tee@gmail.com>
2025-08-01 22:27:42 +0900

    Add eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -17,8 +18,24 @@ const eslintConfig = [
   ...tseslint.configs.recommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
       "no-console": "warn",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^@?\\w"],
+            ["^(@|components)(/.*|$)"],
+            ["^\\u0000"],
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
     },
   },
   eslintConfigPrettier,
