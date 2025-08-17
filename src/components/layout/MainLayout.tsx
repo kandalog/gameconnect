91198cbd7027f2effcacee79067878be6615c8d9
kandalog <asuasu.tee@gmail.com>
2025-08-17 14:49:43 +0900

    fix: レイアウト全体の最大幅調整

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 export const MainLayout = ({ children }: Props) => {
   return (
-    <div className="px-4">
+    <div className="px-4 max-w-[1200px] mx-auto">
       <div className="container mx-auto flex">
         <div className="min-w-0 flex-1 break-words">{children}</div>
         <div className="w-[200px]">PR</div>
