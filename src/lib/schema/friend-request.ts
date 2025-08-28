709cf2aa3c14fba69998af7fdc6de831f073f330
kandalog <asuasu.tee@gmail.com>
2025-08-28 09:12:19 +0900

    Add: 作成のvalidation追加

--- src/lib/schema/friend-request.ts
+++ src/lib/schema/friend-request.ts
@@ -6,3 +6,12 @@ export const getFriendRequestsSchema = z.object({
     .number({ message: "数値である必要があります" })
     .min(1, "ページ番号は1以上である必要があります"),
 });
+
+export const createFriendRequestSchema = z.object({
+  discordId: z.string({ message: "文字列である必要があります" }).min(1, "必須項目です"),
+  content: z.string().max(255, "メッセージは255文字以内である必要があります"),
+  duration: z
+    .number({ message: "数値である必要があります" })
+    .min(1, "1以上の値である必要があります"),
+  game: z.string({ message: "文字列である必要があります" }).min(1, "必須項目です"),
+});
